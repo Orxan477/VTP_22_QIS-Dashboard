a104454c23a47fc5ffce46f90d6d1d67c3e63d28
Orxan477 <tu201906147@code.edu.az>
2022-12-18 21:04:06 +0400

    Project Created

---
+++ VTP_22_Dashboard/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VTP_22_Dashboard.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
