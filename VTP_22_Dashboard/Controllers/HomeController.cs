bacb660fd738726d068d4cff0e57afb241db043b
Orxan477 <tu201906147@code.edu.az>
2022-12-21 20:32:11 +0400

    Home done

--- VTP_22_Dashboard/Controllers/HomeController.cs
+++ VTP_22_Dashboard/Controllers/HomeController.cs
@@ -1,14 +1,28 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using VTP_22_Dashboard.DAL;
+using VTP_22_Dashboard.ViewModels.Home;
 
 namespace VTP_22_Dashboard.Controllers
 {
+        [Authorize(Roles = "Admin")]
     public class HomeController : Controller
     {
-        [Authorize(Roles = "Admin")]
+        private AppDbContext _context;
+
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            HomeVM home = new HomeVM()
+            {
+                Users = _context.Users.Where(x => !x.IsActive).Skip(1).Count(),
+                Departaments = _context.Departments.Count(),
+                Universities = _context.Universities.Count()
+            };
+            return View(home);
         }
     }
 }
---
+++ VTP_22_Dashboard/ViewModels/Home/HomeVM.cs
@@ -0,0 +1,11 @@
+﻿using VTP_22_Dashboard.Models;
+
+namespace VTP_22_Dashboard.ViewModels.Home
+{
+    public class HomeVM
+    {
+        public int Universities{ get; set; }
+        public int Departaments{ get; set; }
+        public int Users{ get; set; }
+    }
+}
